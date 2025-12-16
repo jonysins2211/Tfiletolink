dcadf3db7dd6d705469109c7e63d2f1056ca1b73
jonysins2211 <Jonysins11220@gmail.com>
2025-12-17 03:16:14 +0530

    Update web.py

--- Thunder/web.py
+++ Thunder/web.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify
 from Thunder.utils.shortener import resolve_token
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "ML Files Redirect Server Running"
+    return "ML Redirect Server Running"
 
 @app.route("/resolve")
 def resolve():
@@ -18,3 +19,8 @@ def resolve():
         return jsonify({"error": "invalid"}), 403
 
     return jsonify({"url": url})
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
